c57db0916dc643bfadc07d9943a3df04a60ae597
PurvaJ <Purva.Joshi@jmfl.com>
2023-11-30 14:50:21 +0530

    change README.md message

--- lib/demo_native_app.dart
+++ lib/demo_native_app.dart
@@ -2,6 +2,7 @@ library demo_native_app;
 
 import 'package:flutter/material.dart';
 
+/*
 class AlertBoxPJ {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
@@ -25,6 +26,39 @@ class AlertBoxPJ {
                 MaterialButton(
                   color: Colors.white30,
                   child: const Text('close alert 123'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
+*/
+class AlertBoxPJ {
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "context is null!!");
+    assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: Text('close alert'),
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
